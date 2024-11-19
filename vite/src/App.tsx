5fffb4397d262a6b60c5202adf45f4147bbe9dfd
natanieltam <94886538+natanieltam@users.noreply.github.com>
2024-11-19 15:17:51 +0700

    notification in admin dashboard

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -3,26 +3,39 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PatientForm from "./PatientForm";
 import ObservationForm from "./ObservationForm";
 import ConditionForm from "./ConditionForm";
-import "./index.css";
-import SignIn from "./Authentication/SignIn";
+import SignIn from "./Components/SignIn";
 import Profile from "./Profile";
+import Admin from "./Components/Admin";
+import AdminDashboard from "./Components/AdminDashboard";
+import { AuthProvider } from "./Components/useAuth";
+import RegisterAdmin from "./Components/RegisterAdmin";
+import { PrivateRoute } from "./Components/PrivateRoute";
 import Index from "./index";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* <Route path="/index" element={<Index />} /> */}
-          <Route path="/" element={<SignIn />} />
-          <Route path="/index" element={<Index />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/observation" element={<ObservationForm />} />
-          <Route path="/patient" element={<PatientForm />} />
-          <Route path="/condition" element={<ConditionForm />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            {/* <Route path="/index" element={<Index />} /> */}
+            <Route path="/" element={<SignIn />} />
+            <Route path="/index" element={<Index />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/registeradmin" element={<RegisterAdmin />} />
+            <Route path="/admin" element={<Admin />} />
+            {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+            <Route
+              path="/admin/dashboard"
+              element={<PrivateRoute component={AdminDashboard} />}
+            />
+            <Route path="/observation" element={<ObservationForm />} />
+            <Route path="/patient" element={<PatientForm />} />
+            <Route path="/condition" element={<ConditionForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 };
 
