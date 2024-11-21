1388f74fe3374ae782ec613bbc3ceb889b4d188b
Felix <94885800+pelikk@users.noreply.github.com>
2024-11-21 17:27:18 +0700

    perf: :zap: Improve structure web

--- vite/src/Components/SignIn.tsx
+++ vite/src/Components/SignIn.tsx
@@ -1,24 +1,165 @@
 import { auth, db, provider } from "../Firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import GoogleButton from "react-google-button";
 import "react-toastify/dist/ReactToastify.css";
 
+const GeneratePHRID = () => {
+  return (
+    Math.random().toString(36).substr(2, 5) +
+    Math.floor(1000 + Math.random() * 9000)
+  );
+};
+
+const createUserDoc = async (user: User, phrId: string) => {
+  const userDocRef = doc(db, "Users", phrId);
+  const userDoc = await getDoc(userDocRef);
+
+  if (!userDoc.exists()) {
+    await setDoc(userDocRef, {
+      resourceType: "Patient",
+      email: user.email,
+      name: [
+        {
+          use: "official",
+          family: user.displayName?.split(" ")[1],
+          given: [user.displayName?.split(" ")[0]],
+        },
+      ],
+    });
+    toast.update("Update more data for Patient", {
+      position: "top-center",
+    });
+  }
+};
+
+const createPHRDoc = async (user: User) => {
+  const phrDocRef = doc(db, "PHR", user.uid);
+  const phrDoc = await getDoc(phrDocRef);
+  const phrId = GeneratePHRID();
+
+  if (phrDoc.exists()) {
+    const phrData = phrDoc.data();
+    if (user.uid === phrData.googleId) {
+      toast.info("User already has a PHR ID", {
+        position: "top-center",
+      });
+      window.location.href = "/profile";
+    }
+  } else {
+    await setDoc(phrDocRef, {
+      googleId: user.uid,
+      phrId: phrId,
+      fhirId: "",
+    });
+    toast.success("Successfully create new PHR ID", {
+      position: "top-center",
+    });
+
+    return phrId;
+  }
+};
+
 const SignIn = () => {
-  const googleSignIn = () => {
-    signInWithPopup(auth, provider).then(async (result) => {
+  // const googleSignIn = () => {
+  //   signInWithPopup(auth, provider).then(async (result) => {
+  //     const credential = GoogleAuthProvider.credentialFromResult(result);
+
+  //     const token = credential?.idToken;
+  //     console.log(token, "google token");
+
+  //     const user = result.user;
+  //     console.log(user, "user");
+
+  //     if (user) {
+  //       try {
+  //         // Validate google ID token using backend server
+  //         const response = await fetch(
+  //           `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`,
+  //           {
+  //             method: "POST",
+  //             headers: {
+  //               "Content-Type": "application/json",
+  //             },
+  //             body: JSON.stringify({ token }),
+  //           }
+  //         );
+  //         if (response.ok) {
+  //           const data = await response.json();
+  //           console.log(data, "tokenInfo");
+  //         } else {
+  //           throw new Error("Token validation failed");
+  //         }
+  //       } catch (error) {
+  //         console.error("Error validating token:", error);
+  //         toast.error("Failed to sign in. Please try again.", {
+  //           position: "top-center",
+  //         });
+  //       }
+
+  //       // create PHR path for user
+  //       const phrDocRef = doc(db, "PHR", user.uid);
+  //       const phrDoc = await getDoc(phrDocRef);
+  //       const phrId = `${
+  //         Math.random().toString(36).substr(2, 5) +
+  //         Math.floor(1000 + Math.random() * 9000)
+  //       }`;
+  //       const userDocRef = doc(db, "Users", phrId);
+  //       const userDoc = await getDoc(userDocRef);
+
+  //       // check if google ID already has a PHR ID
+  //       if (phrDoc.exists()) {
+  //         const phrData = phrDoc.data();
+  //         if (user.uid === phrData.googleId) {
+  //           toast.info("User already has a PHR ID", {
+  //             position: "top-center",
+  //           });
+  //           // window.location.href = "/profile";
+  //           window.location.href = "/patient";
+  //         }
+  //       } else {
+  //         await setDoc(phrDocRef, {
+  //           googleId: user.uid,
+  //           phrId: phrId,
+  //           fhirId: "",
+  //         });
+  //         toast.success("Successfully create new PHR ID", {
+  //           position: "top-center",
+  //         });
+
+  //         // check if user already has a Patient document
+  //         if (!userDoc.exists()) {
+  //           await setDoc(userDocRef, {
+  //             resourceType: "Patient",
+  //             email: user.email,
+  //             name: [
+  //               {
+  //                 use: "official",
+  //                 family: user.displayName?.split(" ")[1],
+  //                 given: [user.displayName?.split(" ")[0]],
+  //               },
+  //             ],
+  //           });
+  //           toast.update("Update more data for Patient", {
+  //             position: "top-center",
+  //           });
+  //         }
+  //         window.location.href = "/patient";
+  //         // window.location.href = "/profile";
+  //       }
+  //     }
+  //   });
+  // };
+  const googleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
       const credential = GoogleAuthProvider.credentialFromResult(result);
-
       const token = credential?.idToken;
-      console.log(token, "google token");
-
       const user = result.user;
-      console.log(user, "user");
 
       if (user) {
         try {
-          // Validate google ID token using backend server
           const response = await fetch(
             `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`,
             {
@@ -42,59 +183,18 @@ const SignIn = () => {
           });
         }
 
-        // create PHR path for user
-        const phrDocRef = doc(db, "PHR", user.uid);
-        const phrDoc = await getDoc(phrDocRef);
-        const phrId = `${
-          Math.random().toString(36).substr(2, 5) +
-          Math.floor(1000 + Math.random() * 9000)
-        }`;
-        const userDocRef = doc(db, "Users", phrId);
-        const userDoc = await getDoc(userDocRef);
-
-        // check if google ID already has a PHR ID
-        if (phrDoc.exists()) {
-          const phrData = phrDoc.data();
-          if (user.uid === phrData.googleId) {
-            toast.info("User already has a PHR ID", {
-              position: "top-center",
-            });
-            // window.location.href = "/profile";
-            // window.location.href = "/patient";
-            window.location.href = "/patient";
-          }
-        } else {
-          await setDoc(phrDocRef, {
-            googleId: user.uid,
-            phrId: phrId,
-            fhirId: "",
-          });
-          toast.success("Successfully create new PHR ID", {
-            position: "top-center",
-          });
-
-          // check if user already has a Patient document
-          if (!userDoc.exists()) {
-            await setDoc(userDocRef, {
-              resourceType: "Patient",
-              email: user.email,
-              name: [
-                {
-                  use: "official",
-                  family: user.displayName?.split(" ")[1],
-                  given: [user.displayName?.split(" ")[0]],
-                },
-              ],
-            });
-            toast.update("Update more data for Patient", {
-              position: "top-center",
-            });
-          }
-          window.location.href = "/";
-          // window.location.href = "/profile";
+        const phrId = await createPHRDoc(user);
+        if (phrId) {
+          await createUserDoc(user, phrId);
+          window.location.href = "/patient";
         }
       }
-    });
+    } catch (error) {
+      console.error("Error signing in:", error);
+      toast.error("Failed to sign in. Please try again.", {
+        position: "top-center",
+      });
+    }
   };
 
   return (
--- vite/src/Profile.tsx
+++ vite/src/Profile.tsx
@@ -3,37 +3,87 @@ import { toast } from "react-toastify";
 import { auth, db } from "./Firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
-import "./index.css";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+
+const fetchUserDetails = async (uid: string) => {
+  try {
+    const phrDoc = await getDoc(doc(db, "PHR", uid));
+    const phrId = phrDoc.data()?.phrId;
+    const userDoc = await getDoc(doc(db, "Users", phrId));
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      return {
+        resourceType: "Patient",
+        fhirId: userData?.fhirId,
+        given: userData?.name?.[0]?.given?.[0],
+        family: userData?.name?.[0]?.family,
+        gender: userData?.gender,
+        birthDate: userData?.birthDate,
+        phone: userData?.telecom?.[0]?.value,
+        email: userData?.telecom?.[1]?.value,
+      };
+    } else {
+      console.error("User does not exist in the database");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    return null;
+  }
+};
 
 const Profile: React.FC = () => {
   const [userDetails, setUserDetails] = useState<any>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  const getUserDetails = async () => {
-    try {
-      onAuthStateChanged(auth, async (user) => {
-        if (user) {
-          const phrDoc = await getDoc(doc(db, "PHR", user.uid));
-          const phrId = phrDoc.data()?.phrId;
-          const userDoc = await getDoc(doc(db, "Users", phrId));
-          if (userDoc.exists()) {
-            setUserDetails(userDoc.data());
-          } else {
-            console.error("User does not exist in the database");
-          }
-        }
-        setIsLoading(false);
-      });
-    } catch (error) {
-      console.error("Error fetching user details:", error);
-      setIsLoading(false);
-    }
-  };
+  // const getUserDetails = async () => {
+  //   onAuthStateChanged(auth, async (user) => {
+  //     if (user) {
+  //       const phrDoc = await getDoc(doc(db, "PHR", user.uid));
+  //       const phrId = phrDoc.data()?.phrId;
+  //       const userDoc = await getDoc(doc(db, "Users", phrId));
+  //       if (userDoc.exists()) {
+  //         const googlePatient = {
+  //           resourceType: "Patient",
+  //           fhirId: userDoc.data()?.fhirId,
+  //           name: [
+  //             {
+  //               use: "official",
+  //               family: userDoc.data()?.name?.[0]?.family,
+  //               given: [userDoc.data()?.name?.[0]?.given?.[0]],
+  //             },
+  //           ],
+  //           gender: userDoc.data()?.gender,
+  //           birthDate: userDoc.data()?.birthDate,
+  //           telecom: [
+  //             {
+  //               system: "phone",
+  //               value: userDoc.data()?.telecom?.[0]?.value,
+  //             },
+  //             {
+  //               system: "email",
+  //               value: userDoc.data()?.telecom?.[1]?.value,
+  //             },
+  //           ],
+  //         };
+  //         console.log(googlePatient, "Patient");
+
+  //         setUserDetails(userDoc.data());
+  //       } else {
+  //         console.error("User does not exist in the database");
+  //       }
+  //     }
+  //   });
+  // };
 
   useEffect(() => {
-    getUserDetails();
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userDetails = await fetchUserDetails(user.uid);
+        setUserDetails(userDetails);
+      }
+      setLoading(false);
+    });
+    return () => unsubscribe();
   }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -81,178 +131,69 @@ const Profile: React.FC = () => {
   }
 
   return (
-    <div className="bg-gradient-to-b from-teal-100 to-white min-h-screen">
-      {/* NAVBAR */}
-      <Navbar />
-      <main className="container mx-auto px-4 py-8">
-        <section className="text-center mb-8">
-          <h2 className="text-4xl font-bold text-teal-600">Profile</h2>
-          <p className="text-gray-600 mt-4">
-            Lorem ipsum dolor sit amet consectetur. A nec sit volutpat ut
-            pellentesque adipiscing eros mi. Vulputate scelerisque vitae quis
-            aliquam magna. Euismod diam mauris neque platea non sapien. Lectus
-            neque urna facilisi eu dolor sit iaculis molestie risus.
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#f9f0fa",
+        color: "#333",
+      }}
+    >
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : userDetails ? (
+        <div
+          style={{
+            backgroundColor: "fff",
+            padding: "20px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+            textAlign: "center",
+            width: "300px",
+          }}
+        >
+          <h1>Hello, {userDetails?.name?.[0]?.given?.[0]}</h1>
+          <p>
+            <strong>First name:</strong> {userDetails?.name?.[0]?.given?.[0]}
           </p>
-        </section>
-        <section className="bg-white shadow-md rounded-lg p-8">
-          {userDetails ? (
-            <>
-              <form onSubmit={handleSave}>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                  <div>
-                    <label className="block text-gray-600 mb-2">
-                      First Name
-                    </label>
-                    <input
-                      type="text"
-                      name="given"
-                      value={userDetails.name?.[0]?.given?.[0] || ""}
-                      onChange={handleInputChange}
-                      className="w-full border border-gray-300 rounded px-4 py-2"
-                    />
-                  </div>
-                  <div>
-                    <label className="block text-gray-600 mb-2">
-                      Last Name
-                    </label>
-                    <input
-                      type="text"
-                      name="family"
-                      value={userDetails.name?.[0]?.family || ""}
-                      onChange={handleInputChange}
-                      className="w-full border border-gray-300 rounded px-4 py-2"
-                    />
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600 mb-2">Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={
-                      userDetails.telecom?.find(
-                        (t: { system: string }) => t.system === "email"
-                      )?.value || ""
-                    }
-                    onChange={handleInputChange}
-                    className="w-full border border-gray-300 rounded px-4 py-2"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600 mb-2">Phone</label>
-                  <input
-                    type="tel"
-                    name="phone"
-                    value={
-                      userDetails.telecom?.find(
-                        (t: { system: string }) => t.system === "phone"
-                      )?.value || ""
-                    }
-                    onChange={handleInputChange}
-                    className="w-full border border-gray-300 rounded px-4 py-2"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600 mb-2">Address</label>
-                  <input
-                    type="text"
-                    name="line"
-                    value={
-                      userDetails.telecom?.find(
-                        (t: { system: string }) => t.system === "address"
-                      )?.value || ""
-                    }
-                    onChange={handleInputChange}
-                    className="w-full border border-gray-300 rounded px-4 py-2"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600 mb-2">City</label>
-                  <input
-                    type="text"
-                    name="city"
-                    value={
-                      userDetails.telecom?.find(
-                        (t: { system: string }) => t.system === "city"
-                      )?.value || ""
-                    }
-                    onChange={handleInputChange}
-                    className="w-full border border-gray-300 rounded px-4 py-2"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600 mb-2">State</label>
-                  <input
-                    type="text"
-                    name="state"
-                    value={
-                      userDetails.telecom?.find(
-                        (t: { system: string }) => t.system === "state"
-                      )?.value || ""
-                    }
-                    onChange={handleInputChange}
-                    className="w-full border border-gray-300 rounded px-4 py-2"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600 mb-2">
-                    Postal Code
-                  </label>
-                  <input
-                    type="text"
-                    name="postalCode"
-                    value={
-                      userDetails.telecom?.find(
-                        (t: { system: string }) => t.system === "postalCode"
-                      )?.value || ""
-                    }
-                    onChange={handleInputChange}
-                    className="w-full border border-gray-300 rounded px-4 py-2"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600 mb-2">Country</label>
-                  <input
-                    type="text"
-                    name="country"
-                    value={
-                      userDetails.telecom?.find(
-                        (t: { system: string }) => t.system === "country"
-                      )?.value || ""
-                    }
-                    onChange={handleInputChange}
-                    className="w-full border border-gray-300 rounded px-4 py-2"
-                  />
-                </div>
-                <div className="flex justify-end space-x-4">
-                  <button
-                    type="button"
-                    className="border border-gray-300 text-gray-600 px-4 py-2 rounded"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="submit"
-                    className="bg-teal-600 text-white px-4 py-2 rounded"
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </form>
-              <button
-                onClick={handleSignOut}
-                className="mt-4 bg-red-600 text-white px-4 py-2 rounded"
-              >
-                Sign Out
-              </button>
-            </>
-          ) : (
-            <h2>Loading...</h2>
-          )}
-        </section>
-      </main>
-      {/* FOOTER */}
-      <Footer />
+          <p>
+            <strong>Last name:</strong> {userDetails?.name?.[0]?.family}
+          </p>
+          <p>
+            <strong>Gender:</strong> {userDetails.gender}
+          </p>
+          <p>
+            <strong>Birth date:</strong> {userDetails.birthDate}
+          </p>
+          <p>
+            <strong>Phone:</strong> {userDetails?.telecom?.[0]?.value}
+          </p>
+          <p>
+            <strong>Email:</strong> {userDetails?.telecom?.[1]?.value}
+          </p>
+          <button
+            style={{
+              padding: "10px 20px",
+              marginTop: "20px",
+              background: "007bff",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+            onClick={handleSignOut}
+          >
+            Sign out
+          </button>
+        </div>
+      ) : (
+        <h2>User not found</h2>
+      )}
     </div>
   );
 };
